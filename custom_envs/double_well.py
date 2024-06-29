6d1e08aaf61cf265ba972b78067fbaf779e8b47d
Ed-Mehrez <41273575+Ed-Mehrez@users.noreply.github.com>
2024-06-29 03:46:26 +0100

    Revert "seed setting + double well experiments"
    This reverts commit 01dfce9b32361c8317e2a0da0f98612457b4af2e.

--- custom_envs/double_well.py
+++ custom_envs/double_well.py
@@ -6,7 +6,7 @@ from gym import spaces
 from gym.envs.registration import register
 from gym.utils import seeding
 
-dt = 0.001
+dt = 0.01
 max_episode_steps = int(20 / dt)
 # max_episode_steps = int(2 / dt)
 
@@ -44,8 +44,8 @@ class DoubleWell(gym.Env):
         ])
 
         # Define cost/reward
-        self.Q = 10*np.eye(self.state_dim)
-        self.R = 0.01*np.eye(self.action_dim)
+        self.Q = np.eye(self.state_dim)
+        self.R = np.eye(self.action_dim)
 
         self.reference_point = np.zeros(self.state_dim)
 
